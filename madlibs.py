1c85037e42bf7d07aa7924fab84494c83b138f11
Zmagnifico3107 <167729351+Zmagnifico3107@users.noreply.github.com>
2024-06-22 21:19:51 +0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,4 @@
+name  = str(input("enter a college class: "))
+adj = str(input("enter an adjective: "))
+act = str(input("enter an activity: "))
+print(" Today, we learned how to ", act, " today at school. But",name," was really ", adj," and came back home earlier.")
